c3d3691945c0e97079649b48726a2aa0da7d8b7f
marcelonicolays <mnicolays@yahoo.com.br>
2024-12-04 23:48:06 -0300

    Resolvido

---
+++ alugames/js/app.js
@@ -0,0 +1,16 @@
+function alterarStatus(id){
+    let conteudo =  document.getElementById(`game-${id}`);
+    let imagem = conteudo.querySelector('.dashboard__item__img');
+    let botao = conteudo.querySelector('.dashboard__item__button');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent ='Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+
+}
