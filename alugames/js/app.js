4069d0c97ef2b672a02e4742e041356a4505e0af
marcelonicolays <mnicolays@yahoo.com.br>
2024-12-05 00:16:22 -0300

    Contador

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,9 +1,11 @@
+let contagem = 0;
+
 function alterarStatus(id){
     let conteudo =  document.getElementById(`game-${id}`);
     let imagem = conteudo.querySelector('.dashboard__item__img');
     let botao = conteudo.querySelector('.dashboard__item__button');
     let nome = conteudo.querySelector('.dashboard__item__name').textContent;
-    
+        
     if (imagem.classList.contains('dashboard__item__img--rented')) {
         if (confirm(`Confirma a devlolução do Boardgame ${nome}?`)) {
         imagem.classList.remove('dashboard__item__img--rented');
@@ -17,6 +19,8 @@ function alterarStatus(id){
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
+        contagem ++;
+        console.log(`Já foram alugados ${contagem} jogos.`);
         } else{
             return;
         }
